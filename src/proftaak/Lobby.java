09d7f7c282ceff04aa0464f0168c2059c3f27c70
Frank <Frank@Frank-Laptop>
2014-03-30 17:04:06 +0200

    Methodes ingevuld/gewijzigd, getters toegevoegd, comments toegevoegd

--- src/proftaak/Lobby.java
+++ src/proftaak/Lobby.java
@@ -6,49 +6,101 @@
 
 package proftaak;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  *
  * @author Sander
  */
-public class Lobby {
+public class Lobby implements Observer{
+    private Gebruiker gebruiker;
     private Chatbox chatbox;
     private List<Gebruiker> gebruikers;
     private List<Spel> spellen;
-    
+
     public Lobby()
     {
-        
+        chatbox = new Chatbox();
     }
     
+    /**
+     * 
+     * @return Alle bestaande spellen.
+     */
+    public List<Spel> getSpellen()
+    {
+        return spellen;
+    }
+    
+    /**
+     * 
+     * @return Alle gebruikers in de lobby.
+     */ 
+    public List<Gebruiker> getGebruikers()
+    {
+        return Collections.unmodifiableList(gebruikers);
+    }
+    
+    /**
+     * Voeg spel toe aan de lobby.
+     * @param naam De naam van het spel.
+     * @param publicGame Openbaar of privé.
+     */
     public void voegSpelToe(String naam, Boolean publicGame)
     {
-        
+        Spel nieuwSpel = new Spel(naam, gebruiker, publicGame, spellen.size()+1);
+        nieuwSpel.addObserver(this);
+        spellen.add(nieuwSpel);
     }
     
-    public void toonHighScores()
+    public List<Integer> toonHighScores()
     {
-        
+        List<Integer> highScores = new ArrayList<>();
+        for(Gebruiker g : gebruikers)
+        {
+            highScores.add(g.getScore());
+        }
+        return Collections.unmodifiableList(highScores);
+        //Bewaard voor de volgende iteratie
     }
     
+    /**
+     * 
+     * @param naam
+     * @param wachtwoord
+     * @return true als het inloggen is geslaagd, anders false
+     */
     public Boolean login(String naam, String wachtwoord)
     {
-        return false;
+        //Bewaard voor volgende iteratie
+        gebruiker = new Gebruiker(naam);
+        gebruikers.add(gebruiker);
+        return true;
     }
     
     public void logout()
     {
-        
+        //Bewaard voor volgende iteratie
     }
     
     public void stuurBericht(String bericht)
     {
-        
+        chatbox.voegBerichtToe(bericht, gebruiker);
     }
     
     public Boolean registreer(String naam, String wachtwoord, String email)
     {
-        return false;
+        //Bewaard voor volgende iteratie
+        return true;
+    }
+    
+    @Override
+    public void update(Observable o, Object arg)
+    {
+        
     }
 }
